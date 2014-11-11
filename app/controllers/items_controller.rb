9d4f3ed03b7e76cd7dc65687f5c8e5ad946f49f7
rakeLL <satbirdd@gmail.com>
2014-11-11 20:58:59 +0800

    redirect to item after created

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -28,7 +28,7 @@ class ItemsController < ApplicationController
 
     respond_to do |format|
       if @item.save
-        format.html { redirect_to @item, notice: 'Item was successfully created.' }
+        format.html { redirect_to items_path }
         format.json { render :show, status: :created, location: @item }
       else
         format.html { render :new }
